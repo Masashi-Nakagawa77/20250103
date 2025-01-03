051ba3d0260ca7c2eda16a4f357b3f6c337da848
Masashi-Nakagawa77 <masashi.nakagawa@accenture.com>
2025-01-04 07:22:09 +0900

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -2,16 +2,23 @@ document.addEventListener('DOMContentLoaded', function() {
     const loginButton = document.getElementById('login-button');
     const headerLeft = document.getElementById('header-left');
 
+    // データレイヤーの初期化
+    window.dataLayer = window.dataLayer || [];
+
     function updateLoginStatus() {
         const loginStatus = localStorage.getItem('loginStatus');
         if (loginStatus === 'logged-in') {
             headerLeft.textContent = '中川雅史';
             loginButton.textContent = 'ログアウト';
             loginButton.value = 'logged-in';
+            // データレイヤーにuser-idを追加
+            window.dataLayer.push({ 'user-id': 'nakagawa' });
         } else {
             headerLeft.textContent = '';
             loginButton.textContent = 'ログイン';
             loginButton.value = 'logged-out';
+            // データレイヤーからuser-idを削除
+            window.dataLayer.push({ 'user-id': null });
         }
     }
 
